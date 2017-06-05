d4acbbc46e419ba5774f465778ce7a8a54ca6334
alinton12 <lintonarron@gmail.com>
2017-06-05 19:25:46 -0400

    added upVote funtionaliity

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,6 +1,23 @@
 app.controller('HomeController', ['$scope', 'suggestions', function($scope, suggestions){
 
-	console.log(suggestions);
+	console.log(suggestions.posts);
 	$scope.posts = suggestions.posts;
-	$scope.title = 'Suggestion Box';
+	$scope.heading = 'Suggestion Box';
+
+	$scope.addSuggestion = function(){
+			console.log($scope.title);
+		if(!$scope.title || $scope.title === ""){
+			return;
+		}else{
+			$scope.posts.push({
+				title: $scope.title,
+				upVotes: 0
+			});
+		}
+		$scope.title = '';
+	};
+
+	$scope.upVote = function(index){
+		$scope.posts[index].upVotes++;
+	};
 }]);
